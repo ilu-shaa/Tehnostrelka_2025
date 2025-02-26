7291427c107665fe3501436cfce5d51334537a4e
ilua T <iluhatrofimov@gmail.com>
2025-02-26 09:58:33 +0500

    clining

--- beta/movies_project/web_service/movies_app/nlp_utils.py
+++
@@ -1,8 +0,0 @@
-"""
-from sentence_transformers import SentenceTransformer
-import numpy as np
-
-model = SentenceTransformer('paraphrase-multilingual-MiniLM-L12-v2')
-
-def get_embedding(text):
-    return model.encode([text], convert_to_numpy=True)[0] if text else np.zeros(384)"""
